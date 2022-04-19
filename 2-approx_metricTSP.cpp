aa9fcf5dd0d51c2d08d74bb7c418839b218b0b73
ashwatiss <nair.131@osu.edu>
2022-04-19 17:43:37 -0400

    2-approx TSP v2

--- 2-approx_metricTSP.cpp
+++ 2-approx_metricTSP.cpp
@@ -7,15 +7,16 @@ using namespace std;
 vector<vector<unsigned long>> adj_list;
 vector<vector<double>> edgeList;
 vector<vector<double>> MSTedges;
-vector<double> MSTnodes, nodeList;
+vector<double> MSTnodes, nodeList, dfs_list;
 map<double, vector<double>> Euler_adjlist, graph_adjlist;
 vector<double> eulerTour;
+int totNodes;
 
 void addEdge(double u, double v, double w) {
     edgeList.push_back({w, u, v});
-    if(find(nodeList.begin(), nodeList.end(), u) == nodeList.end())
+    if (find(nodeList.begin(), nodeList.end(), u) == nodeList.end())
         nodeList.push_back(u);
-    if(find(nodeList.begin(), nodeList.end(), v) == nodeList.end())
+    if (find(nodeList.begin(), nodeList.end(), v) == nodeList.end())
         nodeList.push_back(v);
     graph_adjlist[u].push_back(v);
     graph_adjlist[v].push_back(u);
@@ -29,6 +30,8 @@ void printGraph() {
         }
         cout << endl;
     }
+    for (int i = 0; i < nodeList.size(); i++)
+        cout << nodeList[i] << endl;
 }
 
 void printMST() {
@@ -61,6 +64,7 @@ int ReadIn(string inFile) {
 
     if (myfile.is_open()) {
         getline(myfile, line);
+        getline(myfile, line);
         for (int i = 0; i < line.length(); i++) {
             if (!isdigit(line[i]) && line[i] != '.') {
                 delimiter = line[i];
@@ -69,6 +73,9 @@ int ReadIn(string inFile) {
         }
         myfile.clear();
         myfile.seekg(0);
+        getline(myfile, line);
+        totNodes = stoi(line);
+        cout << "No. of Nodes: " << totNodes << endl;
         while (getline(myfile, line)) {
             for (int i = 0, pos = line.find(delimiter); i < 3; i++, pos = line.find(delimiter)) {
                 edge[i] = stod(line.substr(0, pos));
@@ -82,7 +89,7 @@ int ReadIn(string inFile) {
 }
 
 
-void DepthFirstSearch(vector<double>& dfs_list, double node) {
+void DepthFirstSearch(double node) {
     double neighbor;
     dfs_list.push_back(node);
     for (int i = 0; graph_adjlist.find(node) != graph_adjlist.end() && i < graph_adjlist[node].size() &&
@@ -91,22 +98,37 @@ void DepthFirstSearch(vector<double>& dfs_list, double node) {
         if (find(dfs_list.begin(), dfs_list.end(), neighbor) != dfs_list.end()) {
             continue;
         }
-        DepthFirstSearch(dfs_list, neighbor);
+        DepthFirstSearch(neighbor);
     }
 }
 
 
 bool isConnected() {
-    vector<double> dfs_list;
-    double node = Euler_adjlist.begin()->first;
-    DepthFirstSearch(dfs_list, node);
-    cout << "DFS\n";
-    for (int i = 0; i < dfs_list.size(); i++) {
-        cout << dfs_list[i];
-        if (i != dfs_list.size() - 1)
-            cout << " --> ";
+    double node = nodeList[0];
+    DepthFirstSearch(node);
+    if (dfs_list.size() == nodeList.size())
+        return true;
+    return false;
+}
+
+bool isComplete() {
+    int count;
+    for (int i = 0; i < nodeList.size(); i++) {
+        count = 0;
+        for (int j = 0; j < nodeList.size(); j++) {
+            if (nodeList[i] != nodeList[j]) {
+                for (int e = 0; e < edgeList.size(); e++) {
+                    if ((edgeList[e][1] == nodeList[i] && edgeList[e][2] == nodeList[j]) ||
+                        (edgeList[e][1] == nodeList[j] && edgeList[e][2] == nodeList[i])) {
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
+        if (count != totNodes - 1)
+            return false;
     }
-    cout << endl;
     return true;
 }
 
@@ -136,7 +158,6 @@ bool EulerTour(double node) {
             break;
     }
     return true;
-//    }
 }
 
 void shortcutting() {
@@ -160,7 +181,7 @@ bool find_union(vector<vector<double>>& forest, vector<double> edge) {
     vector<double> res(nodeList.size());
     vector<vector<double>>::iterator node_u;
     vector<double>::iterator inter;
-    if(edge[0] == edge[1])
+    if (edge[0] == edge[1])
         return false;
     sort(edge.begin(), edge.end());
     for (int i = 0, u_idx = -1; i < forest.size(); i++) {
@@ -171,26 +192,14 @@ bool find_union(vector<vector<double>>& forest, vector<double> edge) {
             case 0:
                 continue;
             case 1:
-                for (auto it = res.begin(); it != inter; ++it)
-                    std::cout << ' ' << *it;
-                cout << endl << "----------------------------------" << endl;
                 if (u_idx == -1) {
                     u_idx = i;
                 } else {
                     inter = set_union(forest[u_idx].begin(), forest[u_idx].end(), forest[i].begin(), forest[i].end(),
                                       res.begin());
-                    cout << "union\n";
-                    for (auto it = res.begin(); it != inter; ++it)
-                        std::cout << ' ' << *it;
-                    cout << endl << "----------------------------------" << endl;
                     forest.erase(forest.begin() + i);
                     forest.push_back({res.begin(), inter});
                     forest.erase(forest.begin() + u_idx);
-                    for (int j = 0; j < forest.size(); j++) {
-                        for (int i = 0; i < forest[j].size(); i++)
-                            cout << forest[j][i] << " ";
-                        cout << endl;
-                    }
                     return true;
                 }
 
@@ -221,27 +230,44 @@ void kruskalsMST() {
             Euler_adjlist[edgeList[i][2]].push_back(edgeList[i][1]);
         }
     }
-    for (int j = 0; j < forest.size(); j++) {
-        for (int i = 0; i < forest[j].size(); i++)
-            cout << forest[j][i] << " ";
-        cout << endl;
+}
+
+double getWeight(double u, double v) {
+    for (int i = 0; i < edgeList.size(); i++) {
+        if ((edgeList[i][1] == u && edgeList[i][2] == v) || (edgeList[i][1] == v && edgeList[i][2] == u))
+            return edgeList[i][0];
     }
+    return 0;
 }
 
 
-int main() {
-    ReadIn("D:\\FSU\\COURSES\\Adv Algo\\PROJECT\\DATA\\gr24.txt");
-    isConnected();
+int main(int argc, char *argv[]) {
+    ReadIn("weighted_graph.txt");
+    if (!isConnected()) {
+        cout << "Input is not a connected graph." << endl;
+        return -1;
+    }
+    if (!isComplete()) {
+        cout << "Input graph is not complete." << endl;
+        return -1;
+    }
     sort(edgeList.begin(), edgeList.end());
     kruskalsMST();
+    cout << "MST: ";
     printMST();
-    printEuler();
+    cout << "Euler tour: ";
     for (int i = 0; i < MSTedges.size(); ++i) {
         if (EulerTour(MSTedges[i][1]))
             break;
     }
     cout << endl;
+    cout << "TSP: ";
     shortcutting();
     cout << endl;
+    double cost = 0;
+    for (int i = 0; i < eulerTour.size() - 1; i++) {
+        cost += getWeight(eulerTour[i], eulerTour[i + 1]);
+    }
+    cout << "Cost of TSP: " << cost << endl;
     return 0;
 }
